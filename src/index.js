6aa73da9c90ca6846596c38d6ecd746e782adbb8
Mikonar <78429259+Mikonar@users.noreply.github.com>
2023-01-16 00:27:20 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,89 +1,68 @@
+const coursesEn = ["Hamburger, cream sauce and poiled potates",
+  "Goan style fish curry and whole grain rice",
+  "Vegan Chili sin carne and whole grain rice",
+  "Broccoli puree soup, side salad with two napas",
+  "Lunch baguette with BBQ-turkey filling",
+  "Cheese / Chicken / Vege / Halloum burger and french fries"];
+const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
+  "Goalaista kalacurrya ja täysjyväriisiä",
+  "vegaani Chili sin carne ja täysjyväriisi",
+  "Parsakeittoa,lisäkesalaatti kahdella napaksella",
+  "Lunch baguette with BBQ-turkey filling",
+  "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
 
-'use strict';
+let lang = 'fi';
+let activeMenu = coursesFi;
 
-console.log("hello world");
+const menu = document.querySelector('#menu');
+let menuOrder = 'asc';
 
-  const highestNumber = 10;
-  const lowestNumber = 1;
-  const maxGuessCount = 5;
-  let randomNumber = Math.floor(Math.random() * highestNumber) + lowestNumber;
 
-  const infoText = document.querySelector('.info-text');
-  infoText.textContent = `We have selected a random number between ${lowestNumber} and ${highestNumber}. See if you can guess it in ${maxGuessCount} turns or fewer. We will tell you if your guess was too high or too low.`;
-
-  const guesses = document.querySelector('.guesses');
-  const lastResult = document.querySelector('.lastResult');
-  const lowOrHi = document.querySelector('.lowOrHi');
-  const guessSubmit = document.querySelector('.guessSubmit');
-  const guessField = document.querySelector('.guessField');
-
-  let guessCount = 1;
-  let resetButton;
-
-  let timerStart = Date.now();
-
-  function checkGuess() {
-    const userGuess = Number(guessField.value);
-    if (guessCount === 1) {
-      guesses.textContent = 'Previous guesses: ';
-    }
-
-    guesses.textContent += userGuess + ' ';
-
-    if (userGuess === randomNumber) {
-      lastResult.textContent = 'Congratulations! You got it right!';
-      lastResult.style.backgroundColor = 'green';
-      lowOrHi.textContent = '';
-      setGameOver();
-    } else if (guessCount === maxGuessCount) {
-      lastResult.textContent = '!!!GAME OVER!!!';
-      lowOrHi.textContent = '';
-      setGameOver();
-    } else {
-      lastResult.textContent = 'Wrong!';
-      lastResult.style.backgroundColor = 'red';
-      if(userGuess < randomNumber) {
-        lowOrHi.textContent = 'Last guess was too low!' ;
-      } else if(userGuess > randomNumber) {
-        lowOrHi.textContent = 'Last guess was too high!';
-      }
-    }
-
-    guessCount++;
-    guessField.value = '';
-    guessField.focus();
+const renderMenu = () => {
+  menu.innerHTML = '';
+  for (const item of activeMenu) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    menu.appendChild(li);
   }
+};
 
-  guessSubmit.addEventListener('click', checkGuess);
-
-  function setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Start new game';
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener('click', resetGame);
-
-    let timerEnd = (Date.now() - timerStart) / 1000;
-    //simply using lowOrHi's empty field to fill it with game timer
-    console.log(timerEnd);
-    lowOrHi.textContent = 'Time taken to finish the game: ' + timerEnd + ' seconds.' + 'Total guesses: ' + guessCount;
-
+const changeLanguage = () => {
+  if (lang === 'fi') {
+    lang = 'en';
+    activeMenu = coursesEn;
+  } else {
+    lang = 'fi';
+    activeMenu = coursesFi;
   }
+};
 
-  function resetGame() {
-    timerStart = Date.now();
-    guessCount = 1;
-    const resetParas = document.querySelectorAll('.resultParas p');
-    for (const resetPara of resetParas) {
-      resetPara.textContent = '';
-    }
-
-    resetButton.parentNode.removeChild(resetButton);
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = '';
-    guessField.focus();
-    lastResult.style.backgroundColor = 'white';
-    randomNumber = Math.floor(Math.random() * highestNumber) + lowestNumber;
+const sortCourses = (courses, order) => {
+  let sortedCourses = courses.sort();
+  if (order === 'asc') {
+    menuOrder = 'desc';
+  } else {
+    menuOrder = 'asc';
+    sortedCourses = courses.reverse();
   }
+  return sortedCourses;
+};
+const getRandomDish = (menu) => {
+  const randomIndex = Math.floor(Math.random() * activeMenu.length);
+  alert(activeMenu[randomIndex]);
+};
+
+renderMenu();
+
+document.querySelector('#sort').addEventListener('click', () => {
+  sortCourses(activeMenu, menuOrder);
+  renderMenu();
+});
+document.querySelector('#random').addEventListener('click', () => {
+  getRandomDish();
+});
+
+document.querySelector('#changeLang').addEventListener('click', () => {
+  changeLanguage ();
+  renderMenu();
+});
